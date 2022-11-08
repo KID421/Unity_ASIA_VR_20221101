0a03a8e3909200ca3097ae6810eda01fe5bcad8d
KID <50666207+KID421@users.noreply.github.com>
2022-11-08 17:54:43 +0800

    Update 生成系統資料

---
+++ Assets/Scripts/SpawnSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 生成靶系統
+    /// </summary>
+    public class SpawnSystem : MonoBehaviour
+    {
+        [SerializeField, Header("要發射的雞")]
+        private GameObject prefabChicken;
+        [SerializeField, Header("生成點")]
+        private Transform pointSpawn;
+        [SerializeField, Header("發射力道")]
+        private Vector3 shootPower = new Vector3(500, 1200, 0);
+    }
+}
