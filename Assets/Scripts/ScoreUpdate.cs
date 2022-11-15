896e6362995c13b2d3a21fa6ddd136f9833aaaeb
KID <50666207+KID421@users.noreply.github.com>
2022-11-15 17:47:40 +0800

    Update 添加分數更新管理器

---
+++ Assets/Scripts/ScoreUpdate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace KID
+{
+    /// <summary>
+    /// 分數更新管理器
+    /// </summary>
+    public class ScoreUpdate : MonoBehaviour
+    {
+        [SerializeField, Header("分數文字")]
+        private Text textScore;
+        [SerializeField, Header("每次累加分數"), Range(0, 500)]
+        private int scoreAdd = 100;
+
+        private int scoreTotal;
+    }
+}
--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -13,5 +13,27 @@ namespace KID
         private Transform pointSpawn;
         [SerializeField, Header("發射力道")]
         private Vector3 shootPower = new Vector3(500, 1200, 0);
+        [SerializeField, Header("生成間隔"), Range(0, 5)]
+        private float intervalSpawn = 2;
+
+        // 事件：喚醒事件，播放遊戲後執行一次，大部分處理初始資料
+        private void Awake()
+        {
+            // print("哈囉～");
+            // SpawnObject();
+            // 重複呼叫("方法名稱"，開始時間，間隔)
+            // 0 秒後開始每隔兩秒呼叫 SpawnObject 方法
+            InvokeRepeating("SpawnObject", 0, intervalSpawn);
+        }
+
+        private void SpawnObject()
+        {
+            // 暫存物件 = 生成物件(要生成的物件，座標，角度)
+            // 生成 預製物雞 在 生成點的座標上 角度與生成點相同
+            GameObject temp = Instantiate(prefabChicken, pointSpawn.position, pointSpawn.rotation);
+
+            // 暫存物件.取得元件<剛體>().添加推力(發射力道)；
+            temp.GetComponent<Rigidbody>().AddForce(shootPower);
+        }
     }
 }
